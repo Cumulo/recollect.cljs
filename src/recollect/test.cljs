e3ab6eb2e1289fdf75ea72ea70fc163b7bda9138
jiyinyiyong <jiyinyiyong@gmail.com>
2017-11-11 22:57:15 +0800

    debugging undefined

--- src/recollect/test.cljs
+++ src/recollect/test.cljs
@@ -2,20 +2,24 @@
 (ns recollect.test
   (:require [cljs.test :refer [deftest is run-tests]]
             [recollect.diff :refer [diff-bunch]]
-            [recollect.schema :as schema]))
+            [recollect.schema :as schema]
+            [recollect.util :refer [vec-add seq-add]]))
 
 (deftest
  diff-map-same-id
  ()
  (let [a {:id 1, :data 1}, b {:id 1, :data 2}, options {:key :id}]
-   (is (= (diff-bunch a b options) [[[:data] schema/tree-op-assoc 2]]))))
+   (is (= (diff-bunch a b options) [[schema/tree-op-assoc [:data] 2]]))))
 
 (deftest
  diff-map-by-ids
  ()
  (let [a {:id 1, :data 1}, b {:id 2, :data 1}, options {:key :id}]
-   (is (= (diff-bunch a b options) [[[] schema/tree-op-assoc {:id 2, :data 1}]]))))
+   (is (= (diff-bunch a b options) [[schema/tree-op-assoc [] {:id 2, :data 1}]]))))
 
-(defn main! [] (println "Test loaded!") (run-tests))
+(deftest
+ test-vec-add
+ ()
+ (let [a (list 1 2 3 4), b (list 5 6 7 8)] (is (= (seq-add a b) (list 1 2 3 4 5 6 7 8)))))
 
-(defn reload! [] (println "Code updated!") (run-tests))
+(defn main! [] (println "Test loade!") (run-tests))
