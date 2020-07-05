c8aeaa0e4f0b15de6ef08a8cb08fd1966cfc4142
jiyinyiyong <jiyinyiyong@gmail.com>
2020-07-06 00:21:59 +0800

    switching to new caches that is controlled; alpha release

--- src/recollect/twig.cljs
+++ src/recollect/twig.cljs
@@ -1,10 +1,16 @@
 
 (ns recollect.twig (:require [caches.core :as caches]) (:require-macros [recollect.twig]))
 
+(defonce *twig-caches (caches/new-caches {}))
+
 (defn call-twig-func [f params]
-  (let [xs (concat [f] params), v (caches/access-cache xs)]
-    (if (some? v) v (let [result (apply f params)] (caches/write-cache! xs result) result))))
+  (let [xs (concat [f] params), v (caches/access-cache *twig-caches xs)]
+    (if (some? v)
+      v
+      (let [result (apply f params)] (caches/write-cache! *twig-caches xs result) result))))
 
-(defn clear-twig-caches! [] (caches/reset-caches!))
+(defn clear-twig-caches! [] (caches/reset-caches! *twig-caches))
 
-(defn new-twig-loop! [] (caches/new-loop!))
+(defn new-twig-loop! [] (caches/new-loop! *twig-caches))
+
+(defn show-twig-summay [] (caches/show-summary! *twig-caches))
